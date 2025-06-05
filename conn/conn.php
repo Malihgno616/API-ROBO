6b80cfdb21283a836f225012f4962b83d47b5d58
Ackin.Dev <ackinlino123@gmail.com>
2025-06-04 21:56:50 -0300

    Estrutura para conexão para banco de dados

---
+++ conn/conn.php
@@ -0,0 +1,29 @@
+<?php
+
+class Conn {
+    
+    private $host = 'localhost';
+    private $db = 'robot_db';
+    private $user = 'root';
+    private $pass = '';
+    private $charset = 'utf8mb4';
+    private $pdo;
+    private $dsn;
+    public function __construct()
+    {
+        $this->dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+        $this->connect();
+    }
+    public function connect()
+    {
+        return $this->pdo = new PDO($this->dsn, $this->user, $this->pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ]);
+    }
+    public function getConnection()
+    {
+        return $this->pdo;
+    }
+}
